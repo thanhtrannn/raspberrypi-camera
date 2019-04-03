edad3bbf2884c201f3b2d916d73cdbaef462f59a
Thanh Tran <thanh-tran@hotmail.com>
2019-04-03 17:39:46 -0400

    commit - add face recognition

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello")
+import picamera
+import datetime
+from time import sleep
+
+camera = picamera.PiCamera()
+
+#naming file with unique timestamp
+timestamp = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+try:
+    sleep(3)
+finally:
+    camera.capture("/home/pi/PycharmProjects/raspberrypi-camera/photoDatabase/" + timestamp + ".jpg")
+    camera.close()
