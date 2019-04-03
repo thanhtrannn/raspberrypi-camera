2dd9a7514758f9044ad95cc0c6e65d6e1d7f42d5
Thanh Tran <thanh-tran@hotmail.com>
2019-04-03 18:12:45 -0400

    commit - facial recognition working with david and thanh in database

--- test.py
+++ test.py
@@ -10,5 +10,8 @@ timestamp = datetime.datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 try:
     sleep(3)
 finally:
+    camera.start_preview()
+    sleep(1)
     camera.capture("/home/pi/PycharmProjects/raspberrypi-camera/photoDatabase/" + timestamp + ".jpg")
+    camera.stop_preview()
     camera.close()
