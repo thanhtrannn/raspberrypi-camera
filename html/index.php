757de8daed785774605ae88818a97d78d657f56b
Thanh Tran <49013066+thanhtrannn@users.noreply.github.com>
2019-04-08 09:44:20 -0400

    Add files via upload

--- html/index.php
+++ html/index.php
@@ -2,11 +2,14 @@
 <html>
 <?php
     // if form is submitted, run python script with text as message
-     if (isset($_POST['text-to-speech'])) {
+    if (isset($_POST['text-to-speech'])) {
         $text = $_POST['message'];
 	// send argument to terminal with message
         //$runScript = exec("python3 /home/pi/PycharmProjects/raspberrypi-camera/textToSpeech.py '$text'");
-        
+    }
+    if (isset($_POST['add-face'])) {
+    	$name = $_POST['addFace'];
+	$runScript = exec("sudo python3 /home/pi/PycharmProjects/raspberrypi-camera/encode_faces.py -n '$name'");
 	$runScript;
     }
 ?>
@@ -22,14 +25,25 @@
 <body>
 <div class="container-fluid">
     <h1>Front Doorbell</h1>
-    <img src="http://192.168.1.14:8081/" onerror="this.src='image/camera.png'" width="640" height="480"/>
+    <img src="http://142.222.68.4:8081/" onerror="this.src='image/camera.png'" width="640" height="480"/>
     <form method="post">
-        <div class=col-sm-5>
-            <label for="message">Message:</label>
-            <textarea class="form-control" rows="5" name="message"></textarea>
-            <br>
-            <button class="btn" name="text-to-speech">Send</button>
-        </div>
+	<div class="form-group">
+        	<div class="col-sm-5">
+            		<label for="message">Message:</label>
+            		<textarea class="form-control" rows="5" name="message"></textarea>
+            		<br>
+            		<button class="btn" name="text-to-speech">Send</button>
+        	</div>
+	</div>
+	<div class="col-sm-7"></div>
+	<div class="form-group">
+		<div class="col-sm-5">
+			<label for="addFace">Add to Database:</label>
+            		<input class="form-control" placeholder="firstname_lastname" name="addFace"></textarea>
+            		<br>
+            		<button class="btn" name="add-face">Add</button>
+		</div>
+	</div>
     </form>
  </body>
 </html>
